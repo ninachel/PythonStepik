f8019b1060afbf3b0fd81308a7ba57be396563bf
ninachelysheva <nccccc17@gmail.com>
2020-03-25 01:15:21 +0300

    -

--- Числа по спирали.py
+++ Числа по спирали.py
@@ -1,15 +1,17 @@
-num = int(input())
-matrix = [[0] * num for j in range(num)]
-i, j = 0, 0
-for k in range(1, num ** 2 + 1):
-    matrix[i][j] = k
-    if i + j < num - 1 and i <= j + 1:
-        j += 1
-    elif i + j >= num - 1 and i < j:
-        i += 1
-    elif i + j > num - 1:
-        j -= 1
-    else:
-        i -= 1
-for i in range(num):
-    print(*matrix[i])
+def num(n):
+    dx, dy = 1, 0
+    x, y = 0, 0
+    matrix = [[None] * n for _ in range(n)]
+    for i in range(1, n**2+1):
+        matrix[x][y] = i
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < n and 0 <= ny < n and not matrix[nx][ny]:
+            x, y = nx, ny
+        else:
+            dx, dy = -dy, dx
+            x, y = x+dx, y+dy
+    for x in list(zip(*matrix)):
+        print(*x)
+
+
+num(int(input()))
